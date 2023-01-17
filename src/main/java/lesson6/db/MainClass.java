dec617237c2228c892d101b72a9f5257dc2711e5
Witcher200 <94950726+Witcher200@users.noreply.github.com>
2023-01-17 09:36:03 +0300

    Update MainClass.java

--- src/main/java/lesson6/db/MainClass.java
+++ src/main/java/lesson6/db/MainClass.java
@@ -1,94 +1,95 @@
-package lesson6.db;
-
-import lesson6.db.dao.ProductsMapper;
-import lesson6.db.model.CategoriesExample;
-import lesson6.db.model.ProductsExample;
+import db.dao.CategoriesMapper;
+import db.dao.ProductsMapper;
+import db.model.Categories;
+import db.model.CategoriesExample;
+import db.model.ProductsExample;
+import lombok.SneakyThrows;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainClass {
 
-	  public static void main(String[] args) throws IOException {
-			String resource = "mybatisConfig.xml";
-			InputStream is = Resources.getResourceAsStream(resource);
+public class lesson6 {
 
-			System.out.println(countNumberOfAllCategories());
-		//	new SqlSessionFactoryBuilder().build(is);
-			db.dao.CategoriesMapper categoriesMapper = getCategoriesMapper(resource);
-			CategoriesExample cat_example = new CategoriesExample();
-			System.out.println(categoriesMapper.countByExample(cat_example));
+    public static void main(String[] args) throws IOException {
+        //    SqlSessionFactory sqlSessionFactory;
+        String resource = "mybatis-config.xml";
+        InputStream is = Resources.getResourceAsStream(resource);
+  //      sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+//        sqlSessionFactory.
+        System.out.println(countNumberOfAllCategories());
+        CategoriesMapper categoriesMapper = getCategoriesMapper(resource);
+        CategoriesExample cat_example = new CategoriesExample();
+        System.out.println(categoriesMapper.countByExample(cat_example));
 
-			long cat_id = 1;
-			System.out.println("by ID - " + categoriesMapper.selectByPrimaryKey(cat_id).getTitle());
+       //Создание новой категории
+        Categories new_cat = new Categories();
 
-			System.out.println("select всех категорий");
-			categoriesMapper.selectByExample(cat_example).forEach(category ->
-				System.out.println(category.getId() + " " + category.getTitle()));
+        new_cat.setId(3L);
+        new_cat.setTitle("Clothes");
+        categoriesMapper.insert(new_cat);
 
-			ProductsExample pr_examp = new ProductsExample();
-			db.dao.ProductsMapper productMap = getProductsMapper(resource);
-			long id = 3;
-			System.out.println( productMap.selectByPrimaryKey(id).getTitle() + " " + productMap.selectByPrimaryKey(id).getPrice() );
+        //select по ID
+        System.out.println("by ID - " + categoriesMapper.selectByPrimaryKey(1L).getTitle());
 
-			System.out.println(productMap.countByExample(pr_examp));
+        System.out.println("select всех категорий");
+        categoriesMapper.selectByExample(cat_example).forEach(category ->
+                System.out.println(category.getId() + " " + category.getTitle()));
 
-			id = 10;
-			productMap.deleteByPrimaryKey(id);
-			//        System.out.println(productMap.selectByPrimaryKey(id).toString()) ;
+      /*  cat_id = 12;
+        categoriesMapper.deleteByPrimaryKey(cat_id);
+        categoriesMapper.selectByExample(cat_example).forEach(category ->
+                System.out.println(category.getId() + " " + category.getTitle()));
+*/
+        ProductsExample pr_examp = new ProductsExample();
+        ProductsMapper productMap = getProductsMapper(resource);
+        long id = 3;
+        System.out.println( productMap.selectByPrimaryKey(id).getTitle() + " " + productMap.selectByPrimaryKey(id).getPrice() );
 
-			try {
-				  productMap.selectByPrimaryKey(id).getId();
-			} catch (NullPointerException e) {
-				  System.out.println("ID " + id + " not found");
-			}
-
-			//Создание новой категории
-			db.model.Categories new_cat = new db.model.Categories();
-			cat_id = 10;
-
-			new_cat.setId(cat_id);
-			new_cat.setTitle("Clothes");
-			//categoriesMapper.insert(new_cat);
-
-			db.model.Products new_prod = new db.model.Products();
-			cat_id = 10;
-
-			new_prod.setId(cat_id);
-			new_prod.setTitle("wwwww");
-			productMap.insert(new_prod);
+        System.out.println(productMap.countByExample(pr_examp));
 
 
-			//select по ID
+         id = 10;
+        productMap.deleteByPrimaryKey(id);
+        //        System.out.println(productMap.selectByPrimaryKey(id).toString()) ;
 
-	  }
+      try {
+            productMap.selectByPrimaryKey(id).getId();
+        } catch (NullPointerException e) {
+            System.out.println("ID " + id + " not found");
+        }
 
-	  static Integer countNumberOfAllCategories() throws IOException {
-			String resource = "mybatisConfig.xml";
-			db.dao.CategoriesMapper categoriesMapper = getCategoriesMapper(resource);
-			db.model.CategoriesExample example = new db.model.CategoriesExample();
-			return Math.toIntExact(categoriesMapper.countByExample(example));
-	  }
+    }
 
-	  static db.dao.CategoriesMapper getCategoriesMapper(String resource) throws IOException {
-			InputStream inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			sqlSessionFactory.openSession();
-			SqlSession session1 = sqlSessionFactory.openSession();
-			return session1.getMapper(db.dao.CategoriesMapper.class);
-	  }
+    public static Integer countNumberOfAllCategories()  throws IOException {
+        String resource = "mybatis-config.xml";
+        CategoriesMapper categoriesMapper = getCategoriesMapper(resource);
+        CategoriesExample example = new CategoriesExample();
+        return Math.toIntExact(categoriesMapper.countByExample(example));
+    }
 
-	  static db.dao.ProductsMapper getProductsMapper(String resource) throws IOException {
-			InputStream inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			sqlSessionFactory.openSession();
-			SqlSession session1 = sqlSessionFactory.openSession();
-			return session1.getMapper(db.dao.ProductsMapper.class);
-	  }
+    private static CategoriesMapper getCategoriesMapper(String resource)  throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new
+                SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession();
+        return session.getMapper(CategoriesMapper.class);
+    }
+
+    private static ProductsMapper getProductsMapper(String resource)  throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new
+                SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession();
+        return session.getMapper(ProductsMapper.class);
+    }
+
 }
-
 
